9f5ca2efdc6ce513130a8a4da2a7319c3226e404
csemken <csemken@users.noreply.github.com>
2022-05-10 16:47:39 +0200

    🐛 FIX: Prolific redirect records completion time

--- otree_survey/prolific.py
+++ otree_survey/prolific.py
@@ -5,6 +5,9 @@ from otree.views import Page
 class ProlificRedirect(Page):
     """Redirect back to Prolific"""
 
+    def is_displayed(self):
+        return not self.participant._is_bot
+
     def get(self):
         if "prolific_completion_code" in self.participant.vars:
             code = self.participant.vars["prolific_completion_code"]
@@ -14,4 +17,9 @@ class ProlificRedirect(Page):
             raise LookupError(
                 "Could not find 'prolific_completion_code' in participant.vars or session.config"
             )
+
+        self._record_page_completion_time()
+        # do not increment the number of pages, this way when a participant uses the link again they will still be redirected to Prolific
+        # self._increment_index_in_pages()
+
         return redirect(f"https://app.prolific.co/submissions/complete?cc={code}")
